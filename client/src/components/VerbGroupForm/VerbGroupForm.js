4383e08efe1c2b572361f5be6397658eb60ef00c
jacqlynb <jacqlyn.bender@gmail.com>
2021-05-25 15:46:25 -0700

    create VerbGroupForm component + unit tests

---
+++ client/src/components/VerbGroupForm/VerbGroupForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './VerbGroupForm.css';
+
+export function VerbGroupForm(props) {
+  const verbGroups = ['er', 'ir', 'irregular-ir', 'irregular-re', 'irregular-oir'];
+  const verbGroupMarkup = verbGroups.map(verbGroup => {
+    return (
+      <div key={verbGroup}>
+        <input type="radio"
+               id={verbGroup}
+               name="verb-group"
+               value={verbGroup}
+               defaultChecked={verbGroup === 'er' ? true : false}
+               onChange={props.onSelect} />
+        <label htmlFor={verbGroup}>
+            {verbGroup.includes('irregular')
+              ? verbGroup.replace('irregular-', 'irrégulier « ') + ' »'
+              : '« ' + verbGroup + ' »'}
+        </label>
+      </div>
+    );
+  });
+
+  return (
+    <div className="verb-group-form">
+    <h4>Group:</h4>
+    {verbGroupMarkup}
+    </div>
+  );
+};
