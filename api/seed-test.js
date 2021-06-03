b469a299dd90a0bda7dbb6afda2ac5d7a483852a
jacqlynb <jacqlyn.bender@gmail.com>
2021-06-03 10:27:57 -0700

    add utf8_bin collation to all columns in test data

--- api/seed-test.js
+++ api/seed-test.js
@@ -12,9 +12,10 @@ const connection = mysql.createConnection({
 connection.query('SELECT * FROM test', (error, results) => {
   if (results == null) {
     throw new Error("test table doesn't exist");
-  }
-  if (results.length === 0) {
+  } else if (results.length === 0) {
     seed(connection);
+    connection.end();
+  } else {
     connection.end();
   }
 });
