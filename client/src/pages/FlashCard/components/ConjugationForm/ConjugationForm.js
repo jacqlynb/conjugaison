cdb233bfc68c2b5c52bb2dcd496eef6b8be9edc9
jacqlynb <jacqlyn.bender@gmail.com>
2021-07-12 09:30:49 -0700

    add styles for conjugation form validation errors

--- client/src/pages/FlashCard/components/ConjugationForm/ConjugationForm.js
+++ client/src/pages/FlashCard/components/ConjugationForm/ConjugationForm.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import classNames from 'classnames';
 import './ConjugationForm.css';
 
 export function ConjugationForm(props) {
-    return (
-        <form onSubmit={props.onSubmit}>
-        <div className="conjugation-prompt">
-          <label className="conjugation-label">{props.pronoun}</label>
-          <input
-            type="text" name="conjugation"
-            onChange={props.onChange}
-            value={props.value}
-            className={props.classNameProp}
-          />
-          <input className="conjugation-submit" type="submit" value="Check" />
-        </div>
-      </form>
-    );
+  console.log('correct', props.correct);
+
+  return (
+      <form onSubmit={props.onSubmit} className="conjugation-form">
+        <label className="conjugation-label">{props.pronoun}</label>
+        <input
+          type="text"
+          name="conjugation"
+          className={classNames({
+            'conjugation-input': true,
+            'conjugation-input--pending': props.correct == null,
+            'conjugation-input--correct': props.correct,
+            'conjugation-input--incorrect': props.correct != null && !props.correct,
+          })}
+          onChange={props.onChange}
+          value={props.value} />
+        <input className="conjugation-submit" type="submit" value="Check" />
+    </form>
+  );
 }
