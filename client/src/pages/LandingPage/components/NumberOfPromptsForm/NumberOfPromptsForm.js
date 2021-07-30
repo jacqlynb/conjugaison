e478c606f137d305fc927918aee0dc2d23b08110
jacqlynb <jacqlyn.bender@gmail.com>
2021-07-29 21:04:39 -0700

    add validation steps for number of prompts form

--- client/src/pages/LandingPage/components/NumberOfPromptsForm/NumberOfPromptsForm.js
+++ client/src/pages/LandingPage/components/NumberOfPromptsForm/NumberOfPromptsForm.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './NumberOfPromptsForm.css';
 
 export function NumberOfPromptsForm(props) {
-  
-    return (
-        <div className="number-of-prompts">
-            <label>Number to practice</label>
-            <input type="number" onChange={props.onSelectNumPrompts}/>
-        </div>
-    );
-  };
+  return (
+    <div className="number-of-prompts">
+      <label className="number-of-prompts__label">Number to practice: </label>
+      <input className="number-of-prompts__input" 
+             type="text" 
+             value={props.numPrompts === 0 ? '' : props.numPrompts}
+             onChange={props.onSelectNumPrompts}/>
+    </div>
+  );
+};
