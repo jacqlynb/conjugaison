b162adb4355c1c82b28a3e2375731630034f9365
jacqlynb <jacqlyn.bender@gmail.com>
2021-07-30 20:01:39 -0700

    create CustomPieChart component for summary page

--- client/src/pages/Summary/Summary.js
+++ client/src/pages/Summary/Summary.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import {useRouteMatch} from 'react-router-dom';
+import {CustomPieChart} from './components';
+import {CustomLink} from '../../components';
 import './Summary.css';
 
 export function Summary(props) {
-    return (
-        <p>Total correct conjugations: {props.numCorrectConjugations}/{props.totalConjugations}</p>
-    )
+  let {url} = useRouteMatch();
+
+  return (
+    <div>
+      <p className="summary__message">Total correct conjugations:</p>
+      <CustomPieChart numCorrectConjugations={props.numCorrectConjugations}
+                      totalConjugations={props.totalConjugations}/>
+      <CustomLink url="/"
+                  route=""
+                  linkStyle="button" 
+                  text="Restart"/>
+    </div>
+  )
 }
